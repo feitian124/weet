32b4d9dda83aa55fcfd20e4cd5aa862d8c254f36
feitian124 <feitian124@gmail.com>
2015-10-15 00:07:12 +0800

    show topics

--- api/controllers/TopicController.js
+++ api/controllers/TopicController.js
@@ -6,6 +6,16 @@
  */
 
 module.exports = {
-	
+  index: function (req, res) {
+    Topic.find().exec(function(err, records) {
+      if (err) {
+        return res.send(400);
+      } else {
+        return res.view({
+          topics: records
+        });
+      }
+    });
+  }
 };
 
--- api/models/Topic.js
+++ api/models/Topic.js
@@ -1,14 +1,20 @@
 /**
-* Topic.js
-*
-* @description :: TODO: You might write a short summary of how this model works and what it represents here.
-* @docs        :: http://sailsjs.org/#!documentation/models
-*/
+ * Topic.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
 
 module.exports = {
-
   attributes: {
-
+    title: {
+      type: 'string',
+      required: true
+    },
+    content: {
+      type: 'string',
+      required: true
+    }
   }
 };
 
